9b9be849ee47a4339a0ee525706e5763ace3205e
ssangjun <uio3001@dongguk.edu>
2023-01-16 18:25:19 +0900

    Chore: 초기설정

---
+++ src/main/java/group1/untile/UntileApplication.java
@@ -0,0 +1,13 @@
+package group1.untile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group1/untile/UntileApplicationTests.java
@@ -0,0 +1,13 @@
+package group1.untile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
